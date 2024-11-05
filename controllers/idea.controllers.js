570fc985cd0aa81d4f8b991b5e57dde8147beca6
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2024-11-05 15:35:49 +0530

    Get Idea based on the id

--- controllers/idea.controllers.js
+++ controllers/idea.controllers.js
@@ -9,10 +9,31 @@ const ideas = require("../models/ideas.model");
  * GET 127.0.0.1:8000/ideaApp/api/v1/ideas 
  * should return the list of all ideas
  */
-
 exports.getAllIdeas = (req,res)=>{           // getAllIdeas function which is available everywhere
     // first read all the ideas from idea model file 
 
     // Return all those ideas
     res.status(200).send(ideas);     // 200 - http status code for okay
+}
+
+/**
+ * Controllers that fetches idea based on the id 
+ * 
+ * GET 127.0.0.1:8000/ideaApp/api/v1/ideas/2
+ */
+exports.getIdeaBasedOnId = (req,res) =>{
+    // we need to first read the idea id from the req path params
+    idea_id = req.params.id;
+
+    // using id, check if the idea with that id is present 
+    if(ideas[idea_id]){     // or can be written also as if(ideas.idea_id)
+        res.status(200).send(ideas[idea_id]);
+    }
+    else{
+        res.status(404).send({      // 404- http status code for not found
+            message: "Idea with the given idea id not found"
+        });
+    }
+
+    // if present return the id
 }
--- routes/idea.routes.js
+++ routes/idea.routes.js
@@ -7,5 +7,10 @@ const idea_controllers = require("../controllers/idea.controllers");
  */
 
 module.exports = (app) =>{   // connect app to the routes
+    // GET 127.0.0.1:8000/ideaApp/api/v1/ideas
     app.get("/ideaApp/api/v1/ideas",idea_controllers.getAllIdeas);   // when this URI called in get call then hand it over to controllers
+
+
+    //GET 127.0.0.1:8000/ideaApp/api/v1/ideas/5
+    app.get("/ideaApp/api/v1/ideas/:id",idea_controllers.getIdeaBasedOnId);
 }
